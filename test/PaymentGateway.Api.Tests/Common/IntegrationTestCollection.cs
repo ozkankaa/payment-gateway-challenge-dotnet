512de7e9733f644ff6535cc5ed8652247203b9ea
ozkankaa <ozkankaa@gmail.com>
2026-06-05 17:29:39 +0100

    Fix tests

--- test/PaymentGateway.Api.Tests/Common/IntegrationTestCollection.cs
+++ test/PaymentGateway.Api.Tests/Common/IntegrationTestCollection.cs
@@ -1,4 +1,6 @@
-﻿[CollectionDefinition("Integration tests", DisableParallelization = true)]
+﻿namespace PaymentGateway.Api.Tests.Common;
+
+[CollectionDefinition("Integration tests", DisableParallelization = true)]
 public sealed class IntegrationTestCollection
 {
 }
